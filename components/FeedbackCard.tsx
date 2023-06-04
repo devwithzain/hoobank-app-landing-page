42bbd3c88705c1e3a229700617ab8184a9bb76f8
Zain Ali <ZainSoftwear11@gmail.com>
2023-06-04 19:59:31 +0500

    Completed

--- components/FeedbackCard.tsx
+++ components/FeedbackCard.tsx
@@ -4,21 +4,40 @@ interface FeedBackProps {
   content: string;
   title: string;
   name: string;
-  img: string;
+  img: string | any;
 }
-const FeedbackCard: React.FC<FeedBackProps> = ({ content, title, name, img }) => (
+const FeedbackCard: React.FC<FeedBackProps> = ({
+  content,
+  title,
+  name,
+  img,
+}) => (
   <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 feedback-card cursor-pointer">
-    <Image src={quotes} alt="double_quotes" className="object-contain" width={42} height={27} />
+    <Image
+      src={quotes}
+      alt="double_quotes"
+      className="object-contain"
+      width={42}
+      height={27}
+    />
     <p className="font-poppins font-normal text-[18px] leading-[32px] text-white my-10">
       {content}
     </p>
     <div className="flex flex-row">
-      <Image src={img} alt={name} className="rounded-full" width={48} height={48} />
+      <Image
+        src={img}
+        alt={name}
+        className="rounded-full"
+        width={48}
+        height={48}
+      />
       <div className="flex flex-col ml-4">
         <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">
           {name}
         </h4>
-        <p className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite">{title}</p>
+        <p className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite">
+          {title}
+        </p>
       </div>
     </div>
   </div>
--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -4,7 +4,8 @@ import FeedbackCard from "./FeedbackCard";
 const Testimonials: React.FC = () => (
   <section
     id="clients"
-    className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+    className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
+  >
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-10 mb-6 relative z-[1]">
       <h1 className={styles.heading2}>
@@ -12,7 +13,8 @@ const Testimonials: React.FC = () => (
       </h1>
       <div className="w-full md:mt-0 mt-6">
         <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-          Everything you need to accept card payments and grow your business anywhere on the planet.
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
         </p>
       </div>
     </div>
@@ -20,7 +22,6 @@ const Testimonials: React.FC = () => (
       {feedback.map((card) => (
         <FeedbackCard key={card.id} {...card} />
       ))}
-
     </div>
   </section>
 );
